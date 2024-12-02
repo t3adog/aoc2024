a693786ee5ac20205f3788c11a1c11775df48d12
Petr Belyakov <p.belyakov@uma.tech>
2024-12-02 16:49:50 +0300

    Day02, part01 is completed

--- src/days/02/day-02.spec.ts
+++ src/days/02/day-02.spec.ts
@@ -1,16 +1,43 @@
 import * as fs from 'fs';
 import { Day02 } from './day-02';
 
-describe('day 02', () => {
-  const day02 = new Day02(
-    fs.readFileSync('./src/days/02/inputs/test.txt', 'utf8'),
-  );
+const day02 = new Day02(
+  fs.readFileSync('./src/days/02/inputs/test.txt', 'utf8'),
+);
 
+describe('day 02', () => {
   it('should solve part01', () => {
-    expect(day02.partOne()).toEqual(0);
+    expect(day02.partOne()).toEqual(2);
   });
 
   it('should solve part02', () => {
     expect(day02.partTwo()).toEqual(1);
+  });
+});
+
+describe('isSafe', () => {
+  it('should return true', () => {
+    expect(day02.isSafe('1 2 3 4 5')).toEqual(true);
+    expect(day02.isSafe('7 6 4 2 1')).toEqual(true);
+    expect(day02.isSafe('1 3 6 7 9')).toEqual(true);
+  });
+
+  it('should return false', () => {
+    expect(day02.isSafe('1 2 7 8 9')).toEqual(false);
+    expect(day02.isSafe('9 7 6 2 1')).toEqual(false);
+    expect(day02.isSafe('1 3 2 4 5')).toEqual(false);
+    expect(day02.isSafe('8 6 4 4 1')).toEqual(false);
+  });
+});
+
+describe('isSorted', () => {
+  it('should return true', () => {
+    expect(day02.isSorted([1, 2, 3, 4, 5])).toEqual(true);
+    expect(day02.isSorted([10, 9, 5, 2, 1].reverse())).toEqual(true);
+  });
+
+  it('should return false', () => {
+    expect(day02.isSorted([1, 2, 10, 4, 5])).toEqual(false);
+    expect(day02.isSorted([10, 9, 1, 2, 1])).toEqual(false);
   });
 });
--- src/days/02/day-02.ts
+++ src/days/02/day-02.ts
@@ -1,15 +1,60 @@
+import { inputStringToArray } from '../../utils/input-handler.util';
 import { AbstractDay } from '../base/abstract-day';
 
 export class Day02 extends AbstractDay {
+  private readonly reports: string[];
   constructor(readonly input: string) {
     super(input);
+    this.reports = inputStringToArray(this.input);
   }
 
   partOne(): number {
-    return 0;
+    let safeReportsCount = 0;
+    for (const report of this.reports) {
+      if (this.isSafe(report)) {
+        safeReportsCount++;
+      }
+    }
+    return safeReportsCount;
   }
 
   partTwo(): number {
     return 1;
+  }
+
+  isSafe(input: string): boolean {
+    let report = input.split(' ').map((item) => {
+      return parseInt(item);
+    });
+
+    if (!this.isSorted(report)) {
+      report = report.reverse();
+      if (!this.isSorted(report)) {
+        return false;
+      }
+    }
+
+    for (let i = 0; i < report.length - 1; i++) {
+      if (!this.isValidLevelDiff(report, i)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  isSorted(report: number[]): boolean {
+    return report.every((v, i, a) => !i || a[i - 1] <= v);
+  }
+
+  isValidLevelDiff(report: number[], index: number): boolean {
+    const value = report[index];
+    const nextStepValue = report[index + 1];
+    if (nextStepValue) {
+      const diff = nextStepValue - value;
+      if (diff <= 0 || diff > 3) {
+        return false;
+      }
+    }
+    return true;
   }
 }
