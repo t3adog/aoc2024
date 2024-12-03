07e938f3dd94bcd87b8131eb3734316897a63d13
Petr Belyakov <p.belyakov@uma.tech>
2024-12-03 14:19:13 +0300

    day03 part01 resolved

--- src/days/03/day-03.spec.ts
+++ src/days/03/day-03.spec.ts
@@ -1,16 +1,26 @@
 import * as fs from 'fs';
 import { Day03 } from './day03';
 
-describe('day 03', () => {
-  const day03 = new Day03(
-    fs.readFileSync('./src/days/template/inputs/test.txt', 'utf8'),
-  );
+const day03 = new Day03(
+  fs.readFileSync('./src/days/03/inputs/test.txt', 'utf8'),
+);
 
+describe('day 03', () => {
   it('should solve part01', () => {
     expect(day03.partOne()).toEqual(161);
   });
 
   it('should solve part02', () => {
     expect(day03.partTwo()).toEqual(1);
+  });
+});
+
+describe('parseCommand', () => {
+  it('should parseCommands', () => {
+    expect(
+      day03.parseCommands(
+        'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))',
+      ).length,
+    ).toEqual(4);
   });
 });
--- src/days/03/day03.ts
+++ src/days/03/day03.ts
@@ -1,15 +1,35 @@
 import { AbstractDay } from '../base/abstract-day';
 
 export class Day03 extends AbstractDay {
+  private readonly mulCommandRegexp: RegExp = /mul\((\d+),(\d+)\)/g;
   constructor(readonly input: string) {
     super(input);
   }
 
   partOne(): number {
-    return 0;
+    let sum = 0;
+    this.parseCommands(this.input).forEach((command) => {
+      sum += this.execMulCommand(command);
+    });
+    return sum;
   }
 
   partTwo(): number {
     return 1;
+  }
+
+  parseCommands(input: string) {
+    const commands = Array.from(
+      input.matchAll(this.mulCommandRegexp),
+      (match) => match[0],
+    );
+    return commands;
+  }
+
+  execMulCommand(command: string): number {
+    const numbers = Array.from(command.matchAll(/\d+/g), (match) =>
+      parseInt(match[0]),
+    );
+    return numbers[0] * numbers[1];
   }
 }
