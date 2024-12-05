8c63024e8997236f3fced0c44f5a47796fe4467c
Petr Belyakov <p.belyakov@uma.tech>
2024-12-05 12:57:00 +0300

    day05 part01 completed

--- src/days/05/day-05.spec.ts
+++ src/days/05/day-05.spec.ts
@@ -6,10 +6,24 @@ const day05 = new Day05(
 );
 describe('day 05', () => {
   it('should solve part01', () => {
-    expect(day05.partOne()).toEqual(0);
+    expect(day05.partOne()).toEqual(143);
   });
 
   it('should solve part02', () => {
     expect(day05.partTwo()).toEqual(1);
+  });
+});
+
+describe('isCorrectUpdate', () => {
+  it('should return true', () => {
+    expect(day05.isCorrectUpdate([75, 47, 61, 53, 29])).toEqual(true);
+    expect(day05.isCorrectUpdate([97, 61, 53, 29, 13])).toEqual(true);
+    expect(day05.isCorrectUpdate([75, 29, 13])).toEqual(true);
+  });
+
+  it('should return false', () => {
+    expect(day05.isCorrectUpdate([75, 97, 47, 61, 53])).toEqual(false);
+    expect(day05.isCorrectUpdate([61, 13, 29])).toEqual(false);
+    expect(day05.isCorrectUpdate([97, 13, 75, 29, 47])).toEqual(false);
   });
 });
--- src/days/05/day-05.ts
+++ src/days/05/day-05.ts
@@ -1,15 +1,67 @@
 import { AbstractDay } from '../base/abstract-day';
 
 export class Day05 extends AbstractDay {
+  readonly rulesBefore: Map<number, number[]> = new Map();
+  readonly rulesAfter: Map<number, number[]> = new Map();
+  readonly updates: number[][];
   constructor(readonly input: string) {
     super(input);
+
+    input
+      .split('\n\n')[0]
+      .split('\n')
+      .map((line) => {
+        const left = line.split('|')[0];
+        const right = line.split('|')[1];
+
+        if (this.rulesBefore.has(parseInt(left))) {
+          this.rulesBefore.get(parseInt(left))?.push(parseInt(right));
+        } else {
+          this.rulesBefore.set(parseInt(left), [parseInt(right)]);
+        }
+
+        if (this.rulesAfter.has(parseInt(right))) {
+          this.rulesAfter.get(parseInt(right))?.push(parseInt(left));
+        } else {
+          this.rulesAfter.set(parseInt(right), [parseInt(left)]);
+        }
+      });
+    this.updates = input
+      .split('\n\n')[1]
+      .split('\n')
+      .map((line) => {
+        return line.split(',').map((item) => {
+          return parseInt(item);
+        });
+      });
   }
 
   partOne(): number {
-    return 0;
+    let result = 0;
+    for (const update of this.updates) {
+      if (this.isCorrectUpdate(update)) {
+        const middle = update[(update.length - 1) / 2];
+        result += middle;
+      }
+    }
+    return result;
   }
 
   partTwo(): number {
     return 1;
+  }
+
+  isCorrectUpdate(update: number[]): boolean {
+    for (let i = 0; i < update.length; i++) {
+      if (this.rulesAfter.has(update[i])) {
+        const rule = this.rulesAfter.get(update[i]);
+        for (let z = i; z < update.length; z++) {
+          if (rule?.includes(update[z])) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
   }
 }
