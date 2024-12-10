f0bba91fd8b4bf280e4136fcf47c4ee7e568ccc8
Petr Belyakov <p.belyakov@uma.tech>
2024-12-10 17:02:07 +0300

    day09 part01 wip

--- src/days/09/day-09.spec.ts
+++ src/days/09/day-09.spec.ts
@@ -1,16 +1,33 @@
 import * as fs from 'fs';
 import { Day09 } from './day-09';
 
+const day09 = new Day09(
+  fs.readFileSync('./src/days/09/inputs/test.txt', 'utf8'),
+);
 describe('day 09', () => {
-  const day09 = new Day09(
-    fs.readFileSync('./src/days/09/inputs/test.txt', 'utf8'),
-  );
-
   it('should solve part01', () => {
     expect(day09.partOne()).toEqual(0);
   });
 
   it('should solve part02', () => {
     expect(day09.partTwo()).toEqual(1);
+  });
+});
+
+describe('decryptDiskMap', () => {
+  it('should be decrypted diskMap', () => {
+    expect(day09.decryptDiskMap('2333133121414131402')).toEqual(
+      '00...111...2...333.44.5555.6666.777.888899',
+    );
+  });
+});
+
+describe('diskMapDefragmentation', () => {
+  it('should be defragmented diskMap', () => {
+    expect(
+      day09.defragmentationDiskMap(
+        '00...111...2...333.44.5555.6666.777.888899',
+      ),
+    ).toEqual('0099811188827773336446555566..............');
   });
 });
--- src/days/09/day-09.ts
+++ src/days/09/day-09.ts
@@ -6,10 +6,61 @@ export class Day09 extends AbstractDay {
   }
 
   partOne(): number {
+    const diskMap = this.decryptDiskMap(this.input);
+    console.log('Decrypted disk map: ', diskMap);
+
+    const defragmentedDiskMap = this.defragmentationDiskMap(diskMap);
+    console.log('Defragmented disk map: ', defragmentedDiskMap);
     return 0;
   }
 
   partTwo(): number {
     return 1;
+  }
+
+  defragmentationDiskMap(diskMapStr: string): string {
+    const diskMap = diskMapStr.split('');
+    for (let i = 0; i < diskMap.length; i++) {
+      if (diskMap[i] === '.') {
+        for (let z = diskMap.length - 1; z > i; z--) {
+          if (diskMap[z] !== '.') {
+            diskMap[i] = diskMap[z];
+            diskMap[z] = '.';
+            break;
+          }
+        }
+      }
+    }
+    return diskMap.join('');
+  }
+
+  decryptDiskMap(encyptedDiskMap: string): string {
+    let diskMap = '';
+    let diskId = 0;
+    for (let i = 0; i < encyptedDiskMap.length; i++) {
+      if (i % 2 === 0) {
+        diskMap = this.concatNSymbols(
+          diskMap,
+          diskId.toString(),
+          Number.parseInt(encyptedDiskMap[i]),
+        );
+        diskId++;
+      } else {
+        diskMap = this.concatNSymbols(
+          diskMap,
+          '.',
+          Number.parseInt(encyptedDiskMap[i]),
+        );
+      }
+    }
+    return diskMap;
+  }
+
+  concatNSymbols(input: string, symbol: string, count: number): string {
+    let result = input;
+    for (let i = 0; i < count; i++) {
+      result += symbol;
+    }
+    return result;
   }
 }
