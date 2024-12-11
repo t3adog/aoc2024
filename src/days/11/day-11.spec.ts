33216ed87e4db24c25c2e6d189b5b18dc26d1c15
Petr Belyakov <p.belyakov@uma.tech>
2024-12-11 10:23:38 +0300

    day11 part01 completed

--- src/days/11/day-11.spec.ts
+++ src/days/11/day-11.spec.ts
@@ -7,10 +7,84 @@ const day11 = new Day11(
 
 describe('day 01', () => {
   it('should solve part01', () => {
-    expect(day11.partOne()).toEqual(0);
+    expect(day11.partOne()).toEqual(55312);
   });
 
   it('should solve part02', () => {
     expect(day11.partTwo()).toEqual(1);
+  });
+});
+
+describe('binkNtimes', () => {
+  it('blink test 01', () => {
+    const stones = ['125', '17'];
+    expect(day11.blinkNTimes(stones, 6).length).toEqual(22);
+  });
+
+  it('blink test 01', () => {
+    const stones = ['125', '17'];
+    expect(day11.blinkNTimes(stones, 25).length).toEqual(55312);
+  });
+});
+
+describe('blink', () => {
+  it('blink test 01', () => {
+    expect(day11.blink(['0', '1', '10', '99', '999']).join(' ')).toEqual(
+      '1 2024 1 0 9 9 2021976',
+    );
+  });
+
+  it('blink test 02', () => {
+    expect(day11.blink(['125', '17']).join(' ')).toEqual('253000 1 7');
+  });
+
+  it('blink test 03', () => {
+    expect(day11.blink(['253000', '1', '7']).join(' ')).toEqual(
+      '253 0 2024 14168',
+    );
+  });
+
+  it('blink test 04', () => {
+    expect(day11.blink(['253', '0', '2024', '14168']).join(' ')).toEqual(
+      '512072 1 20 24 28676032',
+    );
+  });
+
+  it('blink test 05', () => {
+    expect(
+      day11.blink(['512072', '1', '20', '24', '28676032']).join(' '),
+    ).toEqual('512 72 2024 2 0 2 4 2867 6032');
+  });
+
+  it('blink test 06', () => {
+    expect(
+      day11
+        .blink(['512', '72', '2024', '2', '0', '2', '4', '2867', '6032'])
+        .join(' '),
+    ).toEqual('1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32');
+  });
+
+  it('blink test 07', () => {
+    expect(
+      day11
+        .blink([
+          '1036288',
+          '7',
+          '2',
+          '20',
+          '24',
+          '4048',
+          '1',
+          '4048',
+          '8096',
+          '28',
+          '67',
+          '60',
+          '32',
+        ])
+        .join(' '),
+    ).toEqual(
+      '2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2',
+    );
   });
 });
--- src/days/11/day-11.ts
+++ src/days/11/day-11.ts
@@ -1,15 +1,45 @@
 import { AbstractDay } from '../base/abstract-day';
 
 export class Day11 extends AbstractDay {
+  stones: string[];
   constructor(readonly input: string) {
     super(input);
   }
 
   partOne(): number {
-    return 0;
+    let stones = this.input.split(' ');
+    stones = this.blinkNTimes(stones, 25);
+    return stones.length;
   }
 
   partTwo(): number {
     return 1;
+  }
+
+  blinkNTimes(stones: string[], n: number): string[] {
+    for (let i = 0; i < n; i++) {
+      //console.log(`Stones before: ${stones.join(' ')}`);
+      stones = this.blink(stones);
+      //console.log(`Stones after: ${stones.join(' ')}`);
+    }
+    return stones;
+  }
+
+  blink(stones: string[]): string[] {
+    const newStones: string[] = [];
+    for (let i = 0; i < stones.length; i++) {
+      const stone = stones[i];
+      if (stone === '0') {
+        newStones.push('1');
+      } else if (stone.length % 2 === 0) {
+        newStones.push(stone.substring(0, stone.length / 2));
+        newStones.push(
+          Number.parseInt(stone.substring(stone.length / 2)).toString(),
+        );
+      } else {
+        newStones.push((Number.parseInt(stone) * 2024).toString());
+      }
+    }
+    return newStones;
   }
 }
